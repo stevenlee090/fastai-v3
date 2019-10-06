81d1e059d63d07d2e45fd9cf4eb56712776ba61d
Steven Lee <stevenlee090@hotmail.com>
2019-10-06 13:04:09 +1100

    Render Project: Modified model file and class names.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/1b7ikf91zau0z8p/export-konosuba.pkl?dl=1'
+export_file_name = 'export-konosuba.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['kazuma', 'megumin', 'darkness']
 path = Path(__file__).parent
 
 app = Starlette()
